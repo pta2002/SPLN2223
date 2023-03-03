1c15b9698d527973034b68cfc34175d2b1259f2d
Pedro Alves <pta2002@pta2002.com>
2023-03-03 15:46:01 +0000

    TPC2 transformer

--- TPC2/parser.py
+++ TPC2/parser.py
@@ -1,14 +1,13 @@
-from lark import Lark
+from lark import Lark, Transformer, Discard
 
 grammar = r"""
 start: termo*
-termo: cabecalho nota* traducao* NEWLINE
+termo: cabecalho var* traducao* NEWLINE
 
-cabecalho: "# " ID ": " PALAVRA SEP area SEP TIPO NEWLINE
-nota: /\w+/ " - " ANY NEWLINE
+cabecalho: "# " ID ": " PALAVRA SEP PALAVRA SEP TIPO NEWLINE
+var: TIPO " - " ANY NEWLINE
 traducao: linguagem ": " PALAVRA NEWLINE
 
-area: PALAVRA
 TIPO: /\w+/
 linguagem: LINGUAGEM (" " PAIS)?
 
@@ -22,5 +21,51 @@ SEP: "| "
 ID: /[0-9]+/
 """
 
+
+class MyTransformer(Transformer):
+    def PALAVRA(self, el):
+        return el.strip()
+
+    def SEP(self, _):
+        return Discard
+
+    def NEWLINE(self, _):
+        return Discard
+
+    def cabecalho(self, el):
+        id = el[0].value
+        palavra = el[1]
+        area = el[2]
+        tipo = el[3].value
+        return {"id": id, "palavra": palavra, "area": area, "tipo": tipo}
+
+    def linguagem(self, el):
+        if len(el) == 1:
+            return (el[0].value, None)
+        else:
+            return (el[0].value, el[1].value)
+
+    def termo(self, el):
+        head = el[0]
+        vars = {}
+        terms = []
+        for e in el[1:]:
+            if e.data == "var":
+                vars[e.children[0].value] = e.children[1].value
+            elif e.data == "traducao":
+                terms.append((e.children[0], e.children[1]))
+        terms.append((("gl", None), head["palavra"]))
+        return {
+            "id": head["id"],
+            "area": head["area"],
+            "type": head["tipo"],
+            "terms": terms,
+            "vars": vars,
+        }
+
+
 parser = Lark(grammar)
-print(parser.parse(open("exemplo.txt").read()).pretty())
+tree = parser.parse(open("exemplo.txt").read())
+
+print("PARSED:")
+print(MyTransformer().transform(tree))
